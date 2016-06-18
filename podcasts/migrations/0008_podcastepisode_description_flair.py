b350deb45b01ff282d0dff5d466ee6f6ef471798
Matt Basta <me@mattbasta.com>
2016-06-18 08:56:10 -0700

    Update migration to not require bitfield dep

--- podcasts/migrations/0008_podcastepisode_description_flair.py
+++ podcasts/migrations/0008_podcastepisode_description_flair.py
@@ -2,19 +2,30 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import bitfield.models
 
 
-class Migration(migrations.Migration):
+try:
+    import bitfield.models
 
-    dependencies = [
-        ('podcasts', '0007_auto_20150901_1613'),
-    ]
 
-    operations = [
-        migrations.AddField(
-            model_name='podcastepisode',
-            name='description_flair',
-            field=bitfield.models.BitField(((b'feedback_link', 'Feedback Link'), (b'site_link', 'Site Link'), (b'powered_by', 'Powered By Pinecast')), default=0),
-        ),
-    ]
+    class Migration(migrations.Migration):
+
+        dependencies = [
+            ('podcasts', '0007_auto_20150901_1613'),
+        ]
+
+        operations = [
+            migrations.AddField(
+                model_name='podcastepisode',
+                name='description_flair',
+                field=bitfield.models.BitField(((b'feedback_link', 'Feedback Link'), (b'site_link', 'Site Link'), (b'powered_by', 'Powered By Pinecast')), default=0),
+            ),
+        ]
+except ImportError:
+    class Migration(migrations.Migration):
+
+        dependencies = [
+            ('podcasts', '0007_auto_20150901_1613'),
+        ]
+
+        operations = []
