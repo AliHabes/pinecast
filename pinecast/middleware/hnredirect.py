6a484a3364136892e322840ef291050a21a2e612
Matt Basta <me@mattbasta.com>
2016-10-22 17:30:34 -0700

    even even even more

--- pinecast/middleware/hnredirect.py
+++ pinecast/middleware/hnredirect.py
@@ -18,8 +18,8 @@ class HostnameRedirect(object):
             return HttpResponse('CHjGQzwy6kL5FSQxIt8ObzsUOu2LQpAIWUWeF6zenyw.djl0LF2Nvo-i_b9QJg_lNk4QjgSlIxCaUuPo3U3R-p0')
 
         elif (hostname == 'pinecast.com.herokudns.com' and
-            req.path == '/.well-known/acme-challenge/MVX7oTixZVZaCcZ-Yk2-hyGj_FgAfrZK4pFzDlYrKjE'):
-            return HttpResponse('MVX7oTixZVZaCcZ-Yk2-hyGj_FgAfrZK4pFzDlYrKjE.djl0LF2Nvo-i_b9QJg_lNk4QjgSlIxCaUuPo3U3R-p0')
+            req.path == '/.well-known/acme-challenge/XNKH55GTiDUDP_h2A7B93bGpIHm5bkKsjRTwWWP4QnA'):
+            return HttpResponse('XNKH55GTiDUDP_h2A7B93bGpIHm5bkKsjRTwWWP4QnA.djl0LF2Nvo-i_b9QJg_lNk4QjgSlIxCaUuPo3U3R-p0')
 
         return HttpResponsePermanentRedirect(
             'https://%s%s' % (PREFERRED_HOSTNAME, req.get_full_path()))
