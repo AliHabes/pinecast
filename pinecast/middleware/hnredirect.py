6748d802935ac4cff49dc93d42a44a1c387c6507
Matt Basta <me@mattbasta.com>
2016-10-22 17:09:13 -0700

    More Let's Encrypt for pinecast.co

--- pinecast/middleware/hnredirect.py
+++ pinecast/middleware/hnredirect.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import HttpResponsePermanentRedirect
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 
 PREFERRED_HOSTNAME = 'pinecast.com'
 
@@ -11,6 +11,11 @@ class HostnameRedirect(object):
         hostname = req.META.get('HTTP_HOST')
         if hostname == PREFERRED_HOSTNAME or hostname.endswith('.' + PREFERRED_HOSTNAME):
             return None
+
+        # More Let's Encrypt
+        if (hostname == 'pinecast.co' and
+            req.path == '/.well-known/acme-challenge/CHjGQzwy6kL5FSQxIt8ObzsUOu2LQpAIWUWeF6zenyw'):
+            return HttpResponse('CHjGQzwy6kL5FSQxIt8ObzsUOu2LQpAIWUWeF6zenyw.djl0LF2Nvo-i_b9QJg_lNk4QjgSlIxCaUuPo3U3R-p0')
 
         return HttpResponsePermanentRedirect(
             'https://%s%s' % (PREFERRED_HOSTNAME, req.get_full_path()))
