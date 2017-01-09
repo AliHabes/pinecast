7fee66d76da6baf30bceaf1cd2775a7d62f807fc
Matt Basta <me@mattbasta.com>
2017-01-08 19:17:02 -0800

    Infinitely better user and podcast admin pages

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from .models import Network, UserSettings
+from podcasts.models import Podcast
 
 
 class UserSettingsInline(admin.StackedInline):
@@ -12,8 +13,21 @@ class UserSettingsInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'profile'
 
+
+class PodcastInline(admin.TabularInline):
+    model = Podcast
+    fk_name = 'owner'
+    readonly_fields = ('slug', 'name', )
+    extra = 0
+    show_change_link = True
+    can_delete = False
+
+    def get_fields(self, request, obj=None):
+        return self.get_readonly_fields(request, obj)
+
 class UserAdmin(UserAdmin):
-    inlines = (UserSettingsInline, )
+    inlines = (UserSettingsInline, PodcastInline)
+    list_display = ('email', 'username', )
 
 
 admin.site.unregister(User)
--- podcasts/admin.py
+++ podcasts/admin.py
@@ -2,8 +2,40 @@ from __future__ import absolute_import
 
 from django.contrib import admin
 
-from .models import Podcast, PodcastEpisode
+from .models import Podcast, PodcastEpisode, PodcastCategory
 
 
-admin.site.register(Podcast)
+class PodcastCategoryInline(admin.TabularInline):
+    model = PodcastCategory
+    fk_name = 'podcast'
+    readonly_fields = ('category', )
+    extra = 0
+
+    def get_fields(self, request, obj=None):
+        return self.get_readonly_fields(request, obj)
+
+class PodcastEpisodeInline(admin.TabularInline):
+    model = PodcastEpisode
+    fk_name = 'podcast'
+    readonly_fields = ('title', 'publish', 'created', 'duration', )
+    extra = 0
+    show_change_link = True
+    can_delete = False
+
+    def get_fields(self, request, obj=None):
+        return self.get_readonly_fields(request, obj)
+
+class PodcastAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name', 'owner_email')
+    inlines = (PodcastEpisodeInline, PodcastCategoryInline, )
+
+
+    # TODO: This is probably not ideal. There's presumably some technical
+    # shizzle-wizzle that can be done to prefetch this with, say, the
+    # get_queryset method.
+    def owner_email(self, obj):
+        return obj.owner.email
+
+
+admin.site.register(Podcast, PodcastAdmin)
 admin.site.register(PodcastEpisode)
