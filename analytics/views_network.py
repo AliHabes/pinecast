90743a59fdb7d762e6762d1927048450f0088a12
Matt Basta <basta@uber.com>
2017-01-27 15:24:22 -0800

    WIP new network analytics endpoints, no url

--- analytics/views_network.py
+++ analytics/views_network.py
@@ -1,14 +1,21 @@
-from __future__ import absolute_import
+from functools import wraps
 
 from .formatter import Format
+from .util import country_code_map, format_ip_list, specific_location_timeframe
 from accounts.models import Network
 from pinecast.helpers import get_object_or_404, json_response
 
 
-@json_response
-def network_listen_history(req):
-    net = get_object_or_404(Network, id=req.GET.get('network_id'), members__in=[req.user])
+def requires_network(view):
+    @wraps(view)
+    def wrapper(req, pod, *args, **kwargs):
+        net = get_object_or_404(Network, id=req.GET.get('network_id'), members__in=[req.user])
+        return view(req, pod, *args, net=net, **kwargs)
+    return json_response(wrapper)
 
+
+@requires_network
+def network_listen_history(req, net):
     pods = net.podcast_set.all()
 
     f = (Format(req, 'listen')
@@ -23,8 +30,8 @@ def network_listen_history(req):
         extra_data={str(p.id): {'slug': p.slug} for p in pods})
 
 
-@json_response
-def network_subscriber_history(req):
+@requires_network
+def network_subscriber_history(req, net):
     net = get_object_or_404(Network, id=req.GET.get('network_id'), members__in=[req.user])
 
     pods = net.podcast_set.all()
@@ -39,3 +46,70 @@ def network_subscriber_history(req):
     return f.format_intervals(
         labels_map={str(p.id): p.name for p in pods},
         extra_data={str(p.id): {'slug': p.slug} for p in pods})
+
+
+@requires_network
+def network_subscriber_locations(req, net):
+    f = (Format(req, 'subscription-country')
+            .select(podcast_f='count')
+            .where(podcast=[str(pod.id) for pod in net.podcast_set.all()])
+            .during('yesterday')
+            .group('country'))
+
+    return f.format_country(label=ugettext('Subscribers'))
+
+@requires_network
+def network_subscriber_locations_specific_source(req, net):
+    return [
+        {'label': country_code_map[x], 'value': x} for x in
+        Format(req, 'subscription-country')
+                .select(v='count')
+                .where(podcast=[str(pod.id) for pod in net.podcast_set.all()])
+                .during('yesterday', force=True)
+                .group('country')
+                .get_resulting_groups()]
+
+@requires_network
+@specific_location_timeframe
+def network_subscriber_locations_specific(req, net, iso_code):
+    f = (Format(req, 'subscription-country')
+            .select(ip=True)
+            .where(
+                podcast=[str(pod.id) for pod in net.podcast_set.all()],
+                country=iso_code,
+            )
+            .during('yesterday', force=True))
+    return format_ip_list(f, 'city')
+
+@requires_network
+def network_listener_locations(req, net):
+    f = (Format(req, 'listen-country')
+            .select(podcast_f='count')
+            .where(podcast=[str(pod.id) for pod in net.podcast_set.all()])
+            .last_thirty()
+            .group('country'))
+
+    return f.format_country(label=ugettext('Listens'))
+
+@requires_network
+def network_listener_locations_specific_source(req, net):
+    return [
+        {'label': country_code_map[x], 'value': x} for x in
+        Format(req, 'listen-country')
+            .select(v='count')
+            .where(podcast=[str(pod.id) for pod in net.podcast_set.all()])
+            .during('sixmonth', force=True)
+            .group('country')
+            .get_resulting_groups()]
+
+@requires_network
+@specific_location_timeframe
+def network_listener_locations_specific(req, net, iso_code):
+    f = (Format(req, 'listen-country')
+            .select(ip=True)
+            .where(
+                podcast=[str(pod.id) for pod in net.podcast_set.all()],
+                country=iso_code,
+            )
+            .during('yesterday'))
+    return format_ip_list(f, 'city')
